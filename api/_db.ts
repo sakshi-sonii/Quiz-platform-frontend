c695e6c48ca12e13c9fefa7d3a9156c9a2f11a59
sakshi-sonii <sakshirs1234@gmail.com>
2025-12-16 13:20:05 +0530

    testing backend file 1

--- api/_db.ts
+++ api/_db.ts
@@ -3,14 +3,13 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("❌ MONGODB_URI not defined");
+  throw new Error("MONGODB_URI not defined");
 }
 
-let isConnected = false;
+let connected = false;
 
 export async function connectDB() {
-  if (isConnected) return;
-
+  if (connected) return;
   await mongoose.connect(MONGODB_URI);
-  isConnected = true;
+  connected = true;
 }
