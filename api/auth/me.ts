c8f6f424b1bcdfe16e4d248b57fd5c7a8057ef86
sakshi-sonii <sakshirs1234@gmail.com>
2025-12-16 14:39:34 +0530

    fixed db imports for testing

--- api/auth/me.ts
+++ api/auth/me.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { connectDB, getUserFromRequest } from "../_db";
+import { connectDB, getUserFromRequest } from "../_db.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- api/users.ts
+++ api/users.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { connectDB, User, getUserFromRequest } from "./_db";
+import { connectDB, User, getUserFromRequest } from "./_db.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*");
