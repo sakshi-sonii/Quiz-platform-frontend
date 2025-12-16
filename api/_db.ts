7e7902d00400a5bb8effce4b41e3410ad286e575
sakshi-sonii <sakshirs1234@gmail.com>
2025-12-16 13:08:48 +0530

    changed _db.ts

--- api/_db.ts
+++ api/_db.ts
@@ -1,24 +1,16 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("❌ MONGODB_URI is missing");
+  throw new Error("❌ MONGODB_URI not defined");
 }
 
-let cached = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
-}
+let isConnected = false;
 
 export async function connectDB() {
-  if (cached.conn) return cached.conn;
+  if (isConnected) return;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then(m => m);
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
+  await mongoose.connect(MONGODB_URI);
+  isConnected = true;
 }
