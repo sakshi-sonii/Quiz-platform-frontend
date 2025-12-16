dd133bcd86b7e2ba6cf7bf10f20d686fd5da91b1
sakshi-sonii <sakshirs1234@gmail.com>
2025-12-16 14:25:17 +0530

    fixed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './storage-mock.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
